653a04741ceccd7506faefe9c633b78f4be8ad3c
noena <noena_deleon@yahoo.com>
2017-11-06 20:09:16 +0800

    Add create action for Tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,5 @@
 class TweetsController < ApplicationController
+  before_action :authenticate_user!
 
   def index
     @tweets = Tweet.all
@@ -7,6 +8,20 @@ class TweetsController < ApplicationController
   def new
     @tweet = Tweet.new
   end
+
+  def create
+    @tweet = current_user.tweets.build(tweet_params)
+     if @tweet.save
+       redirect_to tweets_path
+     else
+       render :new
+     end
+  end
+
+private
+def tweet_params
+  params.require(:tweet).permit(:body)
+end
 
 
 end
