c06f0a5db7ec7b6e084ef892d9c6428b52d43db8
noena <noena_deleon@yahoo.com>
2017-11-07 14:32:27 +0800

    Add update action for Tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -26,6 +26,12 @@ class TweetsController < ApplicationController
     @tweet = Tweet.find(params[:id])
   end
 
+  def update
+    @tweet = Tweet.find(params[:id])
+    @tweet.update(tweet_params)
+    redirect_to tweets_path
+  end
+
 private
 def tweet_params
   params.require(:tweet).permit(:body)
